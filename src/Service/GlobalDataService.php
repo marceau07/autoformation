cc85649f31939ab96284a4b942f59ce8e4b10ad1
Marceau RODRIGUES <marceau0707@gmail.com>
2024-07-19 16:31:39 +0200

    feat: Update Trainee entity to use TEXT type for documents column

--- src/Service/GlobalDataService.php
+++ src/Service/GlobalDataService.php
@@ -2,7 +2,11 @@
 
 namespace App\Service;
 
+use App\Entity\CourseResource;
 use App\Entity\FeedbackCategory;
+use App\Entity\Message;
+use App\Entity\Notification;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 
 class GlobalDataService
@@ -18,5 +22,22 @@ class GlobalDataService
     {
         // Replace YourEntity with the actual entity you want to fetch data from
         return $this->entityManager->getRepository(FeedbackCategory::class)->findAll();
+    }
+
+    public function getNotificationsHomeworksToDo($user)
+    {
+        // return $this->entityManager->getRepository(CourseResource::class)->findAll();
+        return $this->entityManager->getRepository(Notification::class)->findBy(['category' => 'homework_to_do', 'user' => $user]);
+    }
+
+    public function getNotificationsMessages($user)
+    {
+        // return $this->entityManager->getRepository(Message::class)->findAll();
+        return $this->entityManager->getRepository(Notification::class)->findBy(['category' => 'new_message', 'user' => $user]);
+    }
+
+    public function getNotificationsNewCourses($user)
+    {
+        return $this->entityManager->getRepository(Notification::class)->findBy(['category' => 'new_course', 'user' => $user]);
     }
 }
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,6 +3,10 @@
 namespace App\Twig;
 
 use App\Service\GlobalDataService;
+use DateTimeImmutable;
+use Doctrine\ORM\Query\Expr\Math;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
 use Twig\TwigFilter;
@@ -10,16 +14,23 @@ use Twig\TwigFilter;
 class AppExtension extends AbstractExtension implements GlobalsInterface
 {
     private $globalDataService;
+    private $security;
+    private $translator;
 
-    public function __construct(GlobalDataService $globalDataService)
+    public function __construct(GlobalDataService $globalDataService, Security $security, TranslatorInterface $translator)
     {
         $this->globalDataService = $globalDataService;
+        $this->security = $security;
+        $this->translator = $translator;
     }
 
     public function getGlobals(): array
     {
         return [
             'feedbackCategories' => $this->globalDataService->getFeedbackCategories(),
+            'notificationsHomeworksToDo' => $this->globalDataService->getNotificationsHomeworksToDo($this->getCurrentUser()),
+            'notificationsMessages' => $this->globalDataService->getNotificationsMessages($this->getCurrentUser()),
+            'notificationsNewCourses' => $this->globalDataService->getNotificationsNewCourses($this->getCurrentUser()),
         ];
     }
 
@@ -27,7 +38,13 @@ class AppExtension extends AbstractExtension implements GlobalsInterface
     {
         return [
             new TwigFilter('json_decode', [$this, 'jsonDecode']),
+            new TwigFilter('time_ago', [$this, 'timeAgo']),
         ];
+    }
+
+    public function getCurrentUser()
+    {
+        return $this->security->getUser();
     }
 
     /**
@@ -38,5 +55,50 @@ class AppExtension extends AbstractExtension implements GlobalsInterface
     public function jsonDecode($json): mixed
     {
         return json_decode($json);
+    }
+
+    function timeAgo($datetime, $full = false)
+    {
+        $now = new DateTimeImmutable();
+        // Ensure $datetime is a DateTime object
+        if (!$datetime instanceof DateTimeImmutable) {
+            $datetime = new DateTimeImmutable($datetime);
+        }
+        // Calculate the difference
+        $diff = $now->diff($datetime);
+
+        // Define the time periods
+        $units = [
+            'y' => 'year',
+            'm' => 'month',
+            'd' => 'day',
+            'h' => 'hour',
+            'i' => 'minute',
+            's' => 'second',
+        ];
+
+        // Calculate the time difference in each unit
+        $diffStringValue = [];
+        $diffStringText = [];
+        foreach ($units as $key => $value) {
+            if ($diff->$key) {
+                $diffStringValue[] = $diff->$key;
+                $diffStringText[] = $value . ($diff->$key > 1 ? 's' : '');
+            }
+        }
+
+        // Join the time units with appropriate punctuation
+        if (!$full) {
+            $diffStringValue = array_slice($diffStringValue, 0, 1);
+            $diffStringText = array_slice($diffStringText, 0, 1);
+        }
+
+
+        if ($diffStringValue && $diffStringText) {
+            return $this->translator->trans('global.' . implode(', ', $diffStringText), ['%time%' => implode(', ', $diffStringValue)], "messages");
+        } else {
+            return $this->translator->trans('global.now');
+        }
+        // return $diffString ? implode(', ', $diffString) . ' ago' : 'just now';
     }
 }
