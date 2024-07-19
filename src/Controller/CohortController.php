b246bf020247eaa3f44f1cbdfe1820f515d5568a
Marceau RODRIGUES <marceau0707@gmail.com>
2024-07-19 16:28:18 +0200

    ```text feat: Add ROLE_TRAINER authorization to CohortController
    This commit adds the #[IsGranted('ROLE_TRAINER')] attribute to the CohortController class. This attribute ensures that only users with the 'ROLE_TRAINER' role can access the routes defined in the controller. This authorization restriction is necessary to limit access to trainer-specific functionality and protect sensitive data.

--- src/Controller/CohortController.php
+++ src/Controller/CohortController.php
@@ -10,7 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_TRAINER')]
 #[Route('/{_locale}/cohort')]
 class CohortController extends AbstractController
 {
