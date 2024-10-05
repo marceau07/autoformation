3062b0db050f03b365a295914685452126284e21
Marceau RODRIGUES <marceau0707@gmail.com>
2024-10-05 21:14:48 +0200

    Modification fichiers de migration

--- migrations/Version20240927113333.php
+++ migrations/Version20240927113333.php
@@ -20,7 +20,7 @@ final class Version20240927113333 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE trainee ADD tutorial_completed VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE trainee ADD tutorial_completed VARCHAR(255) NOT NULL DEFAULT ""');
     }
 
     public function down(Schema $schema): void
