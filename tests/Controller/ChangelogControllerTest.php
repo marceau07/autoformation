3a82a34414c919826f8443b568ccad9291266ddd
Marceau RODRIGUES <marceau0707@gmail.com>
2025-02-11 05:37:08 +0100

    Génération de tests

---
+++ tests/Controller/ChangelogControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class ChangelogControllerTest extends WebTestCase{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/changelog');
+
+        self::assertResponseIsSuccessful();
+    }
+}
