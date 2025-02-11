c60be77ee46f16b7fbff2785fb8cce9b73fd8326
Marceau RODRIGUES <marceau0707@gmail.com>
2025-02-11 14:40:53 +0100

    Ajout d'un mode de maintenance pour facilement désactiver l'accès au site

---
+++ src/EventListener/CheckMaintenanceListener.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\SiteSettings;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Environment as Twig;
+
+final class CheckMaintenanceListener
+{
+    private $entityManager;
+    private $twig;
+    private $allowedIps;
+
+    public function __construct(EntityManagerInterface $entityManager, Twig $twig)
+    {
+        $this->entityManager = $entityManager;
+        $this->twig = $twig;
+        // localhost, localhost IPV6, local ip, livebox, nordvpn
+        $this->allowedIps = ['127.0.0.1', '::1', '192.168.1.33', '192.168.1.40', '192.168.1.254', '82.65.27.189', '91.205.107.48']; // Liste des IP autorisées
+        // $this->allowedIps = ['127.0.0.1', '192.168.1.33', '192.168.1.40', '192.168.1.254', '82.65.27.189', '138.199.16.217', '92.184.100.140']; // Liste des IP autorisées
+    }
+
+    #[AsEventListener(event: KernelEvents::REQUEST)]
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        $clientIp = $request->headers->get('X-Forwarded-For') ?? $request->getClientIp();
+        // var_dump($clientIp);
+        // var_dump($this->allowedIps);
+        // Vérifie si l'utilisateur est dans les IP autorisées
+        if (in_array($clientIp, $this->allowedIps)) {
+            return; // Autorisé
+        }
+
+        // Vérifie l'état de maintenance dans la base de données
+        $settings = $this->entityManager->getRepository('App\Entity\SiteSettings')->find(1);
+        if(!$settings) {
+            $settings = new SiteSettings();
+            $settings->setMaintenanceMode(true);
+            $this->entityManager->persist($settings);
+            $this->entityManager->flush();
+        }
+        if ($settings && $settings->isMaintenanceMode()) {
+            $content = $this->twig->render('maintenance.html.twig');
+            $response = new Response($content, Response::HTTP_SERVICE_UNAVAILABLE);
+            $event->setResponse($response);
+        }
+    }
+}
