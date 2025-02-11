f395a6a54b09894dedd10a85ea710dba6ea20b25
Marceau RODRIGUES <marceau0707@gmail.com>
2025-02-11 16:01:17 +0100

    Ajout d'un changelog pour avoir accès facilement aux nouveautés

---
+++ src/Controller/ChangelogController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ChangelogRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ChangelogController extends AbstractController
+{    
+    #[Route('/{_locale}/changelogs', name: 'app_changelogs')]
+    public function index(): Response
+    {
+        $url = "https://api.github.com/repos/marceau07/autoformation/releases?per_page=100";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        $releases = curl_exec($curl);
+        curl_close($curl);
+
+        return $this->render('changelog/index.html.twig', [
+            'releases' => $releases,
+        ]);
+    }
+
+    #[Route('/{_locale}/changelog/{version}', name: 'app_changelog_version')]
+    public function changelog(string $version): Response
+    {
+        $url = "https://api.github.com/repos/marceau07/autoformation/releases?per_page=100";
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+        $releases = curl_exec($curl);
+        curl_close($curl);
+
+        return $this->render('changelog/index.html.twig', [
+            'releases' => $releases,
+            'version' => $version,
+        ]);
+    }
+}
