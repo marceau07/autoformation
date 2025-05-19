bd822c2ddb37db0c6092f43fef338cf5fae0c213
Marceau RODRIGUES <marceau0707@gmail.com>
2025-05-19 20:15:59 +0200

    Permet d'autoriser une liste de fichiers même en mode maintenance pour éviter de casser du visuel/fonctionnel

--- src/EventListener/CheckMaintenanceListener.php
+++ src/EventListener/CheckMaintenanceListener.php
@@ -15,6 +15,7 @@ final class CheckMaintenanceListener
     private $entityManager;
     private $twig;
     private $allowedIps;
+    private $allowedRoutes;
 
     public function __construct(EntityManagerInterface $entityManager, Twig $twig)
     {
@@ -23,6 +24,9 @@ final class CheckMaintenanceListener
         // localhost, localhost IPV6, local ip, livebox, nordvpn
         $this->allowedIps = ['127.0.0.1', '::1', '192.168.1.33', '192.168.1.40', '192.168.1.254', '82.64.145.2']; // Liste des IP autorisées
         // $this->allowedIps = ['127.0.0.1', '192.168.1.33', '192.168.1.40', '192.168.1.254', '82.64.145.2', '138.199.16.217', '92.184.100.140']; // Liste des IP autorisées
+        $this->allowedRoutes = [
+            '/css/colors.css',
+        ];
     }
 
     #[AsEventListener(event: KernelEvents::REQUEST)]
@@ -30,11 +34,21 @@ final class CheckMaintenanceListener
     {
         $request = $event->getRequest();
         $clientIp = $request->headers->get('X-Forwarded-For') ?? $request->getClientIp();
+        $currentPath = $request->getPathInfo(); // <-- Récupère l'URL demandée (ex: /css/colors.css)
+
+        // var_dump($currentPath);
         // var_dump($clientIp);
         // var_dump($this->allowedIps);
         // Vérifie si l'utilisateur est dans les IP autorisées
         if (in_array($clientIp, $this->allowedIps)) {
             return; // Autorisé
+        }
+
+        // Autoriser si la route est autorisée
+        foreach ($this->allowedRoutes as $route) {
+            if (str_starts_with($currentPath, $route)) {
+                return;
+            }
         }
 
         // Vérifie l'état de maintenance dans la base de données
